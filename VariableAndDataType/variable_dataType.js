7b2a5b01bc9bf0be98852745a4d1ba204d14109d
fazlerabbi13 <mohammad0132794@gmail.com>
2025-11-26 20:12:24 +0600

    Primitive datatypes represent single values and are immutable.

--- VariableAndDataType/variable_dataType.js
+++ VariableAndDataType/variable_dataType.js
@@ -10,3 +10,15 @@ console.log(n1);
 
 const n2 = 100;
 console.log(n2);
+
+let x = 40.5;
+console.log(x);
+
+let bool  = true;
+console.log(bool);
+
+let empty = null;
+console.log(empty);
+
+let sym = Symbol('unique');
+console.log(sym);
