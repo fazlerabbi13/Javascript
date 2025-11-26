f7eb52865abf2aae4f3cb694f2c4fd4a3fb6d941
fazlerabbi13 <mohammad0132794@gmail.com>
2025-11-26 20:27:15 +0600

    access the last array element using [array.length - 1] index number.

--- Array/createArray.js
+++ Array/createArray.js
@@ -1,5 +1,10 @@
 let arr = [10,50,20,30,40];
 console.log(arr);
 
-console.log(arr[0]);
-console.log(arr[1]);
+// console.log(arr[0]);
+// console.log(arr[1]);
+
+// Accessing the Last Element of an Array
+
+let last = arr[arr.length - 1];
+console.log(last);
