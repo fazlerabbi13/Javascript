0dd6d03b08520f63d7c50a47f9c1629c447dcbf5
fazlerabbi13 <mohammad0132794@gmail.com>
2025-12-01 18:58:32 +0600

    We can convert a number into a string using String() function.

---
+++ TypeConvertion/NumbertoString.js
@@ -0,0 +1,6 @@
+// 2. Number to String
+
+let n = 123;
+console.log(n);
+let str = String(n);
+console.log(str);
---
+++ TypeConvertion/StringtoNumber.js
@@ -0,0 +1,7 @@
+// 1. String to Number
+
+// let str = "123";
+// console.log(str);
+// let n = Number(str);
+// console.log(n);
+
