d0d12297f17a820f2d6eec7b55e71b0ccf2e709f
fazlerabbi13 <mohammad0132794@gmail.com>
2025-12-01 19:17:53 +0600

    converts the boolean value into a number, true becomes 1 and false becomes 0, when we perform the arithematic operations.

--- TypeConvertion/TypeCoercion/StringPlusNumber.js
+++ TypeConvertion/TypeCoercion/StringPlusNumber.js
@@ -1,5 +1,13 @@
-let num = 7;
-let str = "0";
-let res = num + str;
+// 1.String + Number
+// let num = 7;
+// let str = "0";
+// let res = num + str;
+// console.log(res);
+// console.log(typeof(res));
+
+// 2. Boolean + Number
+let bool = true;
+let num = 10;
+let res = num + bool;
 console.log(res);
-console.log(typeof(res));
+
