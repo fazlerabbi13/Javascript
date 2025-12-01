647c8c1b8caae36e372da75d623d372a89bdb431
fazlerabbi13 <mohammad0132794@gmail.com>
2025-12-01 19:03:04 +0600

    We can convert a boolean into a number using Number(), so true becomes 1 and false becomes 0.

--- TypeConvertion/NumbertoString.js
+++ TypeConvertion/NumbertoString.js
@@ -1,7 +1,14 @@
 // 2. Number to String
 
-let n = 123;
-console.log(n);
-let str = String(n);
-// let str = n + "";
-console.log(str);
+// let n = 123;
+// console.log(n);
+// let str = String(n);
+// // let str = n + "";
+// console.log(str);
+
+// 3. Boolean to Number
+
+let bool = true;
+console.log(bool);
+let num = Number(bool);
+console.log(num);
