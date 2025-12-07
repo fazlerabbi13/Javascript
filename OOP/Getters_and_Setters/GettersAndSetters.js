6d17ef05588f47f4ac9a2d2fdfeb775aa490fdd6
fazlerabbi13 <mohammad0132794@gmail.com>
2025-12-07 20:43:47 +0600

    create a class name Person.

---
+++ OOP/Getters_and_Setters/GettersAndSetters.js
@@ -0,0 +1,14 @@
+const person= {
+    name1: "Fazle",
+    name2: "Rabbi",
+    // get Name() {
+    //     return `${this.n1} ${this.n2}`;
+    // },
+    // set Name(name) {
+    //     [this.n1, this.n2] = name.split(" ");
+    // }
+};
+
+console.log(p.Name);
+p.Name = "mohammad ali";
+console.log(p.Name);
