6025cf51770f50aa4ad0aee1de41059506c3666b
fazlerabbi13 <mohammad0132794@gmail.com>
2025-12-07 20:45:19 +0600

    create getter function.and Getters help us in getting the value of the property.

--- OOP/Getters_and_Setters/GettersAndSetters.js
+++ OOP/Getters_and_Setters/GettersAndSetters.js
@@ -1,9 +1,9 @@
 const person= {
     name1: "Fazle",
     name2: "Rabbi",
-    // get Name() {
-    //     return `${this.n1} ${this.n2}`;
-    // },
+    get Name() {
+        return `${this.n1} ${this.n2}`;
+    },
     // set Name(name) {
     //     [this.n1, this.n2] = name.split(" ");
     // }
