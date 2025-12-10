47420967f0554507e0d15e737cae2e76da5c59ff
fazlerabbi13 <mohammad0132794@gmail.com>
2025-12-10 20:05:03 +0600

    create stack class and create constructor for array.

---
+++ Data_Structure/stack/stack.js
@@ -0,0 +1,51 @@
+class Stack {
+    constructor() {
+        this.items = [];
+    }
+
+    // Push element into the stack
+//     push(element) {
+//         this.items.push(element);
+//     }
+
+//     // Remove and return top element
+//     pop() {
+//         if (this.isEmpty()) {
+//             return "Stack is empty!";
+//         }
+//         return this.items.pop();
+//     }
+
+//     // Return top element without removing
+//     peek() {
+//         if (this.isEmpty()) {
+//             return "Stack is empty!";
+//         }
+//         return this.items[this.items.length - 1];
+//     }
+
+//     // Check if stack is empty
+//     isEmpty() {
+//         return this.items.length === 0;
+//     }
+
+//     // Return stack size
+//     size() {
+//         return this.items.length;
+//     }
+
+//     // Print stack
+//     print() {
+//         console.log(this.items.toString());
+//     }
+// }
+
+// // Example usage:
+// const stack = new Stack();
+// stack.push(10);
+// stack.push(20);
+// stack.push(30);
+// stack.print();       // 10,20,30
+// console.log(stack.pop());   // 30
+// console.log(stack.peek());  // 20
+// console.log(stack.size());  // 2
