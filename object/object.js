789b606d86727c4cf160975a7bd0a99bae9de504
fazlerabbi13 <mohammad0132794@gmail.com>
2025-12-13 21:41:26 +0600

    modify object properties.

--- object/object.js
+++ object/object.js
@@ -1,9 +1,15 @@
-let person = {
-    name: "Fazle Rabbi",
-    age: 22,
-    height: 5.6
-};
+// let person = {
+//     name: "Fazle Rabbi",
+//     age: 22,
+//     height: 5.6
+// };
 
-console.log(person.name);
-console.log(person.age);
-console.log(person.height);
+// console.log(person.name);
+// console.log(person.age);
+// console.log(person.height);
+
+let obj = { name: "Rabbi", age: 22 };
+obj.age = 22;  
+obj.city = "Sylhet"; 
+delete obj.name;  
+console.log(obj);
